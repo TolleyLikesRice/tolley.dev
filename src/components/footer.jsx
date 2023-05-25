a1a37e67af3a34a0c39c2a37384b2cdbe38c3791
TolleyLikesRice <tolley@tolley.dev>
2023-05-25 12:45:11 +0100

    Footer - it's a bit shit atm

---
+++ src/components/footer.jsx
@@ -0,0 +1,17 @@
+import Layout from '@/components/layout';
+import { SocialLink } from '@/components/socials';
+import { SOCIAL_LIST } from '@/components/socials';
+
+
+export default function Footer() {
+    return (
+        <footer className="w-screen bg-slate-900 container px-10 py-5">
+            <div className='flex items-center justify-center'>
+                {SOCIAL_LIST.map((item) => (
+                    <SocialLink key={item.text} src={item.src} alt={item.alt} href={item.href} />
+                ))}
+            </div>
+            <div className='text-center w-full pt-5 text-gray-700'>2023 © TolleyLikesRice</div>
+        </footer>
+    )
+}
