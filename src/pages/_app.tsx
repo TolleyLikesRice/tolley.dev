9288cf1cdc01e3a19a01984a9ad5cb9777b6959b
TolleyLikesRice <tolley@tolley.dev>
2023-05-25 13:37:49 +0100

    Now it's typescript

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import '../styles/global.css';
 
+import { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
     return (
         <main className={inter.className}>
             <Component {...pageProps} />
