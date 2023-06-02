6fa248d6f212ed4c6eb8be6e0e94dfb24f1b7a6c
TolleyLikesRice <tolley@tolley.dev>
2023-06-02 15:26:11 +0100

    New Projects Display

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,9 +16,10 @@ import styles from '@/styles/index.module.css';
 import chatbotCover from '../../public/img/projects/chatbot.png';
 import nezzCover from '../../public/img/projects/nezz.png';
 import placeholderCover from '../../public/img/projects/placeholder.webp';
+import placeholder169 from '../../public/img/projects/placeholder-16-9.jpg';
 import websiteCover from '../../public/img/projects/website.png';
 
-const PROJECT_LIST = [
+const HIGHLIGHTS_LIST = [
     {
         title: "FlyDiary",
         description: "A work-in-progress digital logbook solution, since I didn't really like anything else already out there.",
@@ -27,11 +28,11 @@ const PROJECT_LIST = [
         links: [{ src: '/img/socials-row/github-light.png', alt: 'GitHub', href: 'https://github.com/TolleyLikesRice/FlyDiary' }]
     },
     {
-        title: "My Website",
-        description: "This website! Made entry into Next.js, Tailwind CSS, and a few other things.",
+        title: "Tolley's Simple JSON Logger",
+        description: "My own logger for Node.JS, is meant to be part of a wider ecosystem I haven't quite gotten around to coding yet.",
         href: "#",
         img: placeholderCover,
-        links: [{ src: '/img/socials-row/github-light.png', alt: 'GitHub', href: 'https://github.com/TolleyLikesRice/tolley.dev' }]
+        links: [{ src: '/img/socials-row/github-light.png', alt: 'GitHub', href: 'https://github.com/TolleyLikesRice/tsjl-node' }]
     },
     {
         title: "Nezz",
@@ -39,52 +40,49 @@ const PROJECT_LIST = [
         href: "#",
         img: nezzCover,
         links: [{ src: '/img/socials-row/github-light.png', alt: 'GitHub', href: 'https://github.com/TolleyLikesRice/Nezz' }]
-    },
+    }
+]
+
+const PROJECT_LIST = [
     {
         title: "PhasmoHelper",
-        description: "A simple website to help identify ghosts in Phasmophobia, made with plain HTML and JS, using water.css. Went outdated pretty quick with new game updates, and the features of it now built into the game.",
-        href: "#",
-        img: placeholderCover,
-        links: [{ src: '/img/socials-row/github-light.png', alt: 'GitHub', href: 'https://github.com/TolleyLikesRice/PhasmoHelper' }]
+        description: "A simple website to help identify ghosts in Phasmophobia, made with plain HTML and JS, using water.css. Became outdated pretty quick with new game updates, and the features of it now built into the game.",
+        href: "https://github.com/TolleyLikesRice/PhasmoHelper",
+        img: placeholder169,
     },
     {
         title: "KTANEHelper",
         description: "A little helper for Keep Talking and Nobody Explodes, never finished, only had a helper for the \"Memory\" module. Made with plain HTML and JS, using water.css.",
-        href: "#",
-        img: placeholderCover,
-        links: [{ src: '/img/socials-row/github-light.png', alt: 'GitHub', href: 'https://github.com/TolleyLikesRice/KTANEHelper' }]
+        href: "https://github.com/TolleyLikesRice/KTANEHelper",
+        img: placeholder169,
     },
     {
         title: "Just Another Report Plugin",
         description: "A simple report plugin for Minecraft servers, main standout feature being use of GUI, and sending all reports to a discord webhook",
-        href: "#",
-        img: placeholderCover,
-        links: [{ src: '/img/socials-row/github-light.png', alt: 'GitHub', href: 'https://github.com/TolleyLikesRice/JustAnotherReportPlugin' }]
+        href: "https://github.com/TolleyLikesRice/JustAnotherReportPlugin",
+        img: placeholder169,
     },
     {
         title: "Just Another Ban Plugin",
         description: "A snazzy ban plugin for my Minecraft server, never actually got finished, and the server died pretty quick.",
-        href: "#",
-        img: placeholderCover,
-        links: [{ src: '/img/socials-row/github-light.png', alt: 'GitHub', href: 'https://github.com/TolleyLikesRice/JustAnotherBanPlugin' }]
+        href: "https://github.com/TolleyLikesRice/JustAnotherBanPlugin",
+        img: placeholder169,
     },
     {
         title: "SpammyMcSpamFace",
         description: "A really dumb SMS spammer written for Android in Kotlin - a proof of concept more than anything.",
-        href: "#",
-        img: placeholderCover,
-        links: [{ src: '/img/socials-row/github-light.png', alt: 'GitHub', href: 'https://github.com/TolleyLikesRice/SpammyMcSpamFace' }]
+        href: "https://github.com/TolleyLikesRice/SpammyMcSpamFace",
+        img: placeholder169,
     },
     {
         title: "ChatBot",
         description: "My first ever Node.js project, a simple bot for Discord, written using Discord.js",
-        href: "#",
+        href: "https://github.com/TolleyLikesRice/ChatBot",
         img: chatbotCover,
-        links: [{ src: '/img/socials-row/github-light.png', alt: 'GitHub', href: 'https://github.com/TolleyLikesRice/ChatBot' }] 
     },
 ];
 
-function ProjectLinks({ links }) {
+function HighlightLinks({ links }) {
     if (links !== undefined) {
         return (
             <div>
@@ -101,27 +99,49 @@ function ProjectLinks({ links }) {
     );
 }
 
-function ProjectCard({ project }) {
+function HighlightCard({ project }) {
     return (
         <div className='relative w-full pb-4 projectCard'>
             <div className='absolute w-full pb-4 background-blur bg-gray-900/80 projectCardOverlay flex items-center justify-center align-middle'>
                 <div className='text-center m-auto p-2'>
                     <h3 className='text-gray-100 text-2xl font-medium'>{project.title}</h3>
                     <span className='text-gray-400'>{project.description}</span>
-                    <ProjectLinks links={project.links} />
+                    <HighlightLinks links={project.links} />
                 </div>
             </div>
-            <Image className="h-auto w-full rounded-lg shadow-xl shadow-slate-400/40" width={project.width} height={project.height} src={project.img} alt={project.name + " cover photo"} />
+            <Image className="h-auto w-full rounded-lg shadow-xl shadow-slate-500/40" width={project.width} height={project.height} src={project.img} alt={project.name + " cover photo"} />
         </div>
     )
 
 };
+
+function ProjectCard({ project }) {
+    return (
+        <div className='w-full mb-4 bg-slate-900 rounded-lg'>
+            <div className='flex flex-row'>
+                <Image className='h-24 w-44 object-cover m-4' width={project.width} height={project.height} src={project.img} alt={project.name + " photo"}></Image>
+                <div className='flex items-center h-full my-auto pr-4 pl-4'>
+                    <div className='w-full h-full'>
+                        <div className='flex items-center'>
+                            <h3 className='text-gray-100 text-2xl font-medium'>{project.title}</h3>
+                            <a href={project.href} target="_blank" className='ml-3'>
+                                <Image src="/img/socials-row/github.svg" alt="GitHub" width={512} height={512} className="h-5 w-5" />
+                            </a>
+                        </div>
+                        <span className='text-gray-400 text-md'>{project.description}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export default function IndexPage() {
     return (
         <div>
             <Layout>
                 <Head>
+                    { /* eslint-disable-next-line react/no-unescaped-entities */ /* I don't know how to escape this, and I don't care enough to find out */}
                     <title>Hi, I'm Tolley</title>
                 </Head>
                 <Navbar hide={true} />
@@ -155,15 +175,25 @@ export default function IndexPage() {
                 <div id='projects'>
                     <h2 className='text-5xl font-medium text-center text-gray-100'>Projects</h2>
                     <hr className='w-64 h-0.5 mx-auto my-4 border-0 rounded md:my-6 bg-gray-700' />
+                    <h3 className='text-3xl font-medium text-left text-gray-100'>The Highlights</h3>
+                    <hr className='w-64 h-0.5 my-4 !mt-3 border-0 rounded md:my-6 bg-gray-700' />
                     <Masonry
-                        breakpointCols={{ default: 3, 700: 2 }}
+                        breakpointCols={{ default: 3, 700: 1 }}
                         className='masonry-grid'
                         columnClassName='masonry-grid_column'
                     >
-                        {PROJECT_LIST.map((project) => (
-                            <ProjectCard key={project.title} project={project} />
+                        {HIGHLIGHTS_LIST.map((project) => (
+                            <HighlightCard key={project.title} project={project} />
                         ))}
                     </Masonry>
+                    <br /><br />
+                    <h3 className='text-3xl font-medium text-left text-gray-100'>Everything else</h3>
+                    <hr className='w-64 h-0.5 my-4 !mt-3 border-0 rounded md:my-6 bg-gray-700' />
+                    {
+                        PROJECT_LIST.map((project) => (
+                            <ProjectCard key={project.title} project={project} />
+                        ))
+                    }
                 </div>
                 <div id='spacer' className='py-20' />
                 <div id='skills'>
